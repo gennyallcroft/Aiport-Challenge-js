18b235f5c8d53bf12b2e792fc7f832ba30f0063f
Student <student@Makerss-MacBook-Pro.local>
2019-07-23 16:34:40 +0100

    add the take off method

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -13,9 +13,16 @@ describe("Airport", function() {
   it("it has no planes by default", function() {
     expect(airport.planes()).toEqual([]);
   });
+
   it('can clear planes for landing', function(){
     airport.clearForLanding(plane);
     expect(airport.planes()).toEqual([plane]);
   });
+
+  it('can clear planes for take off', function() {
+    airport.clearForTakeoff(plane);
+    expect(airport.planes()).toEqual([]);
+  });
+
 
 });
